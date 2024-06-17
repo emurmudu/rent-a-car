4ed2061e6b4401b19daade036547650e8b53774a
emurmudu <eliasmurmudu@gmail.com>
2024-06-17 20:28:44 +0600

    404 route note found done

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { UserRoutes } from './app/modules/user/user.route';
+import router from './app/routes';
+import routeNotFound from './app/middleware/routeNotFound';
 // const express = require('express')
 const app:Application = express()
 // const port = 3000
@@ -11,7 +13,8 @@ const app:Application = express()
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/auth/signup', UserRoutes)
+// app.use('/api/auth/signup', UserRoutes)
+app.use('/api', router);
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
@@ -21,7 +24,7 @@ app.get('/', (req:Request, res:Response) => {
 //   console.log(`Example app listening on port ${port}`)
 // })
 
-
+app.use(routeNotFound)
 export default app;
 
 
